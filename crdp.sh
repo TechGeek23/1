4931a4b8aaf063c4f9015e70e03d73a2c79c4f13
TechGeek23 <90578012+TechGeek23@users.noreply.github.com>
2021-09-24 17:53:18 -0400

    Create crdp.sh

---
+++ crdp.sh
@@ -0,0 +1,16 @@
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb 
+Sudo apt update  
+
+sudo apt install --assume-yes \    --with-source=chrome-remote-desktop_current_amd64.deb \    chrome-remote-desktop  
+
+sudo DEBIAN_FRONTEND=noninteractive \    apt install --assume-yes xfce4 desktop-base 
+
+echo "xfce4-session" > ~/.chrome-remote-desktop-session 
+
+sudo apt install --assume-yes xscreensaver 
+
+sudo systemctl disable lightdm.service 
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
+
+sudo apt install --assume-yes  google-chrome-stable \    --with-source=google-chrome-stable_current_amd64.deb 
